597d8f09676d8b41eeb566eb649e340599d81c92
Christopher Geiger <christophergeiger3@gmail.com>
2019-08-22 16:44:43 -0400

    Correct and clarify comments/exception messages

--- fixtures/piece.py
+++ fixtures/piece.py
@@ -1,12 +1,12 @@
 
 class Piece:
     def move(self):
-        raise NotImplementedError("Move must be implemented")
+        raise NotImplementedError("move must be implemented")
 
     def set_attacking_squares(self, attacking_squares):
-        raise NotImplementedError("Move must be implemented")
+        raise NotImplementedError("set_attacking_squares must be implemented")
 
-    # Name is the character(s) that appears on the board
+    # Name is the character(s) that appears on the board, on top of the corresponding square
     def __init__(self, name: str, color: str):
         self.name = name
         self.color = color
