686712587c16de6b710de86b1071edbf25b9b0e8
Christopher Geiger <christophergeiger3@gmail.com>
2019-08-22 16:08:21 -0400

    Add test layout for pieces.py

---
+++ tests/testPieces.py
@@ -0,0 +1,24 @@
+import unittest
+
+class PiecesTest(unittest.TestCase):
+
+    def test_pawn(self):
+        raise NotImplementedError("Test not implemented!")
+
+    def test_rook(self):
+        raise NotImplementedError("Test not implemented!")
+
+    def test_knight(self):
+        raise NotImplementedError("Test not implemented!")
+
+    def test_bishop(self):
+        raise NotImplementedError("Test not implemented!")
+
+    def test_queen(self):
+        raise NotImplementedError("Test not implemented!")
+
+    def test_king(self):
+        raise NotImplementedError("Test not implemented!")
+
+if __name__ == '__main__':
+    unittest.main()
