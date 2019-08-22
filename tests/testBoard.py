0ae29ea4783e77828a836469adb489b8efa3fac7
Christopher Geiger <christophergeiger3@gmail.com>
2019-08-22 16:08:05 -0400

    Add test layout for board.py

---
+++ tests/testBoard.py
@@ -0,0 +1,31 @@
+import sys
+import unittest
+
+sys.path.insert(1, '..')
+from board import Square
+from config import BOARD_WIDTH, BOARD_LENGTH
+
+class SquareTest(unittest.TestCase):
+    def test_create_square(self):
+        position = (1, 2)
+        square = Square(position)
+
+    def test_invalid_square(self):
+        position = (BOARD_WIDTH+1, BOARD_LENGTH+1)
+        with self.assertRaises(
+
+'''
+class BoardTest(unittest.TestCase):
+    # TODO: create setUp method to run before each test
+    # TODO: remember to use subtests...
+    def setUp(self):
+        raise NotImplementedError("Set up method not implemented!")
+
+    # TODO: add tests
+'''
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
